234910746df7338664e2feea0d8cd463bddaebc5
pierregaillard1986 <pierregaillard1986@outlook.com>
2018-03-02 16:34:42 +0100

    finish method Divisible tp openclassroom

--- maPremiereApplication/Program.cs
+++ maPremiereApplication/Program.cs
@@ -12,23 +12,23 @@ namespace maPremiereApplication
         static void Main(string[] args)
         {
             //Exercice 1
-            //String prenom = Environment.UserName;
-            //String direBonjour = "Bonjour" + " " + prenom;
-            //String direAurevoir = "Bonsoir" + " " + prenom;
-            //String direBonWeekEnd = "Bon week-end" + " " + prenom;
-            //String day = DateTime.Now.DayOfWeek.ToString();
-            //int hour = DateTime.Now.Hour;
-            //if (day == "Sunday" || day == "Saturday" || (day == "Friday" && hour >= 18) || (day == "Monday" && hour < 9))
-            //{
-            //    Console.WriteLine(direBonWeekEnd);
-            //}
-            //else
-            //{
-            //    if (hour < 9 || hour > 18)
-            //        Console.WriteLine(direAurevoir);
-            //    else
-            //        Console.WriteLine(direBonjour); 
-            //}
+            String prenom = Environment.UserName;
+            String direBonjour = "Bonjour" + " " + prenom;
+            String direAurevoir = "Bonsoir" + " " + prenom;
+            String direBonWeekEnd = "Bon week-end" + " " + prenom;
+            String day = DateTime.Now.DayOfWeek.ToString();
+            int hour = DateTime.Now.Hour;
+            if (day == "Sunday" || day == "Saturday" || (day == "Friday" && hour >= 18) || (day == "Monday" && hour < 9))
+            {
+                Console.WriteLine(direBonWeekEnd);
+            }
+            else
+            {
+                if (hour < 9 || hour > 18)
+                    Console.WriteLine(direAurevoir);
+                else
+                    Console.WriteLine(direBonjour);
+            }
             //Exercice 2
             Console.WriteLine(Calcul(1, 10));
             Console.WriteLine(Calcul(1, 100));
@@ -36,8 +36,9 @@ namespace maPremiereApplication
             //Exercice 3
             List<double> liste = new List<double> { 1.0, 5.5, 9.9, 2.8, 9.6 };
             Console.WriteLine(CalculMoyenne(liste));
-            
+
             //Exercice 4
+            Console.WriteLine(Divisible());
         }
         //Exercice 2
         static int Calcul(int a, int b)
@@ -53,13 +54,34 @@ namespace maPremiereApplication
         static double CalculMoyenne(List<double> a)
         {
             double sum = 0.0;
-            for (int i = 0; i< a.Count; i++)
+            for (int i = 0; i < a.Count; i++)
             {
                 sum += a[i];
             }
-            return sum/a.Count;
+            return sum / a.Count;
         }
         //Exercice 4
-
+        static int Divisible()
+        {
+            List<int> divisibleby3 = new List<int>();
+            List<int> divisibleby5 = new List<int>();
+            List<int> divisibleBy3And5 = new List<int>();
+            foreach (int item in (Enumerable.Range(1, 100).ToList()))
+            {
+                if (item % 3 == 0)
+                {
+                    divisibleby3.Add(item);
+                }
+                if (item % 5 == 0)
+                {
+                    divisibleby5.Add(item);
+                }
+                if ((item % 3 == 0) && (item % 5 == 0))
+                {
+                    divisibleBy3And5.Add(item);
+                }
+            }
+            return divisibleBy3And5.Sum();
+        }
     }
 }
